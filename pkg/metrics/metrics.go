a3c23b52d30349503f17cc32f9d0e32ec3f75857
Cristian Radu <cristian.andrei.radu@gmail.com>
2018-04-30 13:07:17 +0100

    BITE-1491 add some prometheus metrics

---
+++ pkg/metrics/metrics.go
@@ -0,0 +1,17 @@
+package metrics
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+var Deploys = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "deploys_total",
+		Help: "Number of requests to deploy a service caried out by EO.",
+	},
+	[]string{"status"},
+)
+
+func init() {
+	prometheus.MustRegister(Deploys)
+}
