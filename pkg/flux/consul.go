15bda2ccf9f4681c543f1bc11bfdc79c459d2d72
Rathnayake, Supun <Supun.Rathnayake@pearson.com>
2020-06-25 13:00:41 +0530

    Code re-organizations and clean ups

--- pkg/flux/consul.go
+++ pkg/flux/consul.go
@@ -5,6 +5,7 @@ import (
     "encoding/json"
     "encoding/base64"
     "fmt"
+    "strings"
     "io/ioutil"
 )
 
@@ -44,6 +45,65 @@ func ConsulRead(path string) (ConsulValues, error){
     }
     return cvalues, nil
 
+}
+
+// RenderHelmReleasesWithConsul creates a map of serviceIdentifier:HelmRelease yaml
+func RenderHelmReleasesWithConsul(envs *bitesize.EnvironmentsBitesize, regPath string, cvalues ConsulValues) map[string]string {
+	m := make(map[string]string)
+	cv := make(map[string]string)
+
+	for _, env := range envs.Environments {
+		for _, svc := range env.Services {
+
+			if svc.Type == "" { // EO uses nil type for web apps
+				svc.Type = "webservice"
+			}
+			key := fmt.Sprintf("%s", svc.Name)
+
+			   for _, key := range cvalues {
+			   skey := strings.Split(key.Key,"/")
+			   if len(skey) == 2 { // for only Consul key value is defined without any hierarchy
+				cv["namespace"] = skey[0]
+				cv["service"] = "Any"
+				cv["key"] = skey[1]
+				cv["value"] = key.Value
+			   }
+			   if len(skey) > 2 {
+				cv["namespace"] = skey[0]
+				cv["service"] = skey[len(skey)-2]
+				cv["key"] = skey[len(skey)-1]
+				cv["value"] = key.Value
+			   }
+			   if len(skey) > 3 && skey[1] == "namespace" { // for GLP specific mapping
+				cv["namespace"] = skey[2]
+				cv["service"] = skey[3]
+				cv["key"] = skey[len(skey)-1]
+				cv["value"] = key.Value
+			   }
+			   if env.Namespace == cv["namespace"]  {
+				// for where service_name used in Consul is defined as an env variable and is different to BS service name
+				for _, v := range svc.EnvVars {
+					if v.Name == "service_name" && v.Value == cv["service"]{
+						evalue:= bitesize.EnvVar{Name: cv["key"], Value: cv["value"]}
+						svc.EnvVars = append(svc.EnvVars, evalue)
+					}
+				}
+				// for case where Consul values are defined at the top level ad key,value without any hierachy
+				if cv["service"] == "Any" {
+					evalue:= bitesize.EnvVar{Name: cv["key"], Value: cv["value"]}
+					svc.EnvVars = append(svc.EnvVars, evalue)
+				}
+			   }
+			}
+			svc = AddConfigMap(svc)
+			val, err := RenderHelmRelease(env, svc, regPath)
+			if err != nil {
+				panic(err)
+			}
+			m[key] = val
+		}
+	}
+	return m
 }
 
 func AddConfigMap(svc bitesize.Service) (bitesize.Service){
